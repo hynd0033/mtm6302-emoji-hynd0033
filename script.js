5938ae7a91565b4f4e0f9dba3b4958ffa4b0180a
Conner <hynd0033@algonquinlive.com>
2023-10-31 19:38:13 -0400

    final

--- script.js
+++ script.js
@@ -4,75 +4,79 @@
 
 const emoji = [
     {
-        emojiCode:"&#9917",
-        displayCode:"#9917",
-    },
-
-    {
         "emojiCode":"&#9917",
         "displayCode":"#9917",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9918",
+        "displayCode":"#9918",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9749",
+        "displayCode":"#9749",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9889",
+        "displayCode":"#9889",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9940",
+        "displayCode":"#9940",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9971",
+        "displayCode":"#9971",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9977",
+        "displayCode":"#9977",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9973",
+        "displayCode":"#9973",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9970",
+        "displayCode":"#9970",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#10071",
+        "displayCode":"#10071",
     },
 
     {
-        "emojiCode":"&#9917",
-        "displayCode":"#9917",
+        "emojiCode":"&#9925",
+        "displayCode":"#9925",
+    },
+
+    {
+        "emojiCode":"&#9924",
+        "displayCode":"#9924",
     },
 ];
 
-const add = document.getElementById('emoji2')
+ const add = document.getElementById('emoji')
 
 let emojiString2 = "";
 
-for ( let i = 1; i<=12; i++) {
-    emojiString2 += `<p>${emoji.emojiCode}<br><code>${emoji.displayCode}</code></p>`
-}
+for ( let i = 0; i<=12; i++) {
+    emojiString2 += `<p>${emoji[i].emojiCode}<br><code>${emoji[i].displayCode}</code></p>`
+    add.innerHTML = emojiString2
+} 
 
-add.innerHTML = emojiString2
+
+
+
+
 
 /*
 
